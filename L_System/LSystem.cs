59dbf8928f41f782b09284a6ecd90963622ad2a1
sass00n1 <41114110+sass00n1@users.noreply.github.com>
2022-02-16 18:27:31 +0800

    Add files via upload

---
+++ L_System/LSystem.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StartFramework.GamePlay.LSystem
+{
+    [RequireComponent(typeof(Turtle))]
+    public class LSystem : MonoBehaviour
+    {
+        [SerializeField] private string axiom;
+        [SerializeField] private int generation;
+        [SerializeField] private List<Dic> ruleset = new List<Dic>();
+        [SerializeField] private Vector2 posDelta;
+        [SerializeField] private Vector3 angleDelta;
+
+        private string sentence;
+        private Dictionary<string, string> rulesetData = new Dictionary<string, string>();
+
+        [System.Serializable]
+        public struct Dic
+        {
+            public string predecessor;  //前身
+            public string successor;    //接替者
+        }
+
+        private void Start()
+        {
+            //准备数据
+            sentence = axiom;
+            for (int i = 0; i < ruleset.Count; i++)
+            {
+                rulesetData.Add(ruleset[i].predecessor, ruleset[i].successor);
+            }
+            for (int i = 0; i < generation; i++)
+            {
+                GenerateSentence();
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                //乌龟
+                Turtle turtle = GetComponent<Turtle>();
+                turtle.TurtleInit(sentence, transform.position, posDelta, angleDelta);
+                turtle.Draw();
+            }
+        }
+
+        private void GenerateSentence()
+        {
+            sentence = Generate(sentence);
+            //Debug.Log(sentence);
+        }
+
+        private string Generate(string oldSentence)
+        {
+            StringBuilder newSentence = new StringBuilder();
+
+            char[] chars = oldSentence.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (rulesetData.TryGetValue(chars[i].ToString(), out string replacement))
+                {
+                    newSentence.Append(replacement);
+                }
+                else
+                {
+                    newSentence.Append(chars[i]);
+                }
+
+            }
+
+            return newSentence.ToString();
+        }
+    }
+
+}
---
+++ L_System/Turtle.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace StartFramework.GamePlay.LSystem
+{
+    public class Turtle : MonoBehaviour
+    {
+        private string todo;
+        private Vector3 position;
+        private Quaternion quaternion;
+        private Vector3 posDelta;
+        private Vector3 angleDelta;
+        private Stack<TurtleTransform> stack = new Stack<TurtleTransform>();
+
+        private struct TurtleTransform
+        {
+            public Vector3 Position;
+            public Quaternion Quaternion;
+            public TurtleTransform(Vector3 position, Quaternion quaternion)
+            {
+                Position = position;
+                Quaternion = quaternion;
+            }
+        }
+
+        public void TurtleInit(string todo, Vector3 position, Vector3 posDelta, Vector3 angleDelta)
+        {
+            this.todo = todo;
+            this.position = position;
+            this.posDelta = posDelta;
+            this.angleDelta = angleDelta;
+        }
+
+        public void Draw()
+        {
+            char[] chars = todo.ToCharArray();
+
+            IEnumerator enumerator()
+            {
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    char c = chars[i];
+                    if (c == 'F')
+                    {
+                        Vector3 delta = quaternion * posDelta;
+                        Debug.DrawLine(position, position + delta, Color.green, 100);
+                        position += delta;
+                        yield return null;
+                    }
+                    else if (c == '+')
+                    {
+                        quaternion = Quaternion.Euler(quaternion.eulerAngles + angleDelta);
+                    }
+                    else if (c == '-')
+                    {
+                        quaternion = Quaternion.Euler(quaternion.eulerAngles - angleDelta);
+                    }
+                    else if (c == '[')
+                    {
+                        stack.Push(new TurtleTransform(position, quaternion));
+                    }
+                    else if (c == ']')
+                    {
+                        TurtleTransform turtleTransform = stack.Pop();
+                        position = turtleTransform.Position;
+                        quaternion = turtleTransform.Quaternion;
+                    }
+                    else if (c == 'X')
+                    {
+                        //不做任何事情
+                    }
+                }
+            }
+
+            StartCoroutine(enumerator());
+        }
+
+    }
+}
